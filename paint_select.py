6d0258b4e16a88b63188e49294a47e883d528c19
K-410 <iceythe@gmail.com>
2019-04-10 14:43:02 +0200

    Update paint_select.py

--- paint_select.py
+++ paint_select.py
@@ -1,8 +1,7 @@
 import bpy
-
+import bmesh
 from bpy.props import BoolProperty
 from math import isclose
-
 
 bl_info = {
     "name": "Paint Select",
@@ -49,14 +48,42 @@ class VIEW3D_OT_paint_select(bpy.types.Operator):
         return {'RUNNING_MODAL'}
 
     def invoke(self, context, event):
+        bm = bmesh.from_edit_mesh(context.object.data)
+        ts = context.tool_settings
+        msm = ts.mesh_select_mode[:]
+
         self.drag_threshold = context.preferences.inputs.drag_threshold
         self.init_xy = event.mouse_region_x, event.mouse_region_y
+
+        elem = None
+
+        if msm == (False, False, True):
+            elem = bm.faces
+        elif msm == (True, False, False):
+            elem = bm.verts
+        elif msm == (False, True, False):
+            elem = bm.edges
+
+        if not elem:
+            return {'CANCELLED'}
+
+        presel = set(el for el in elem if not el.select)
 
         bpy.ops.view3d.select(
             'INVOKE_DEFAULT',
             extend=False,
             toggle=self.toggle,
             deselect=self.deselect)
+
+        postsel = set(el for el in elem if not el.select)
+
+        diff = postsel - presel
+        if not diff:
+            diff = presel - postsel
+
+        elem.ensure_lookup_table()
+        if self.toggle and diff:
+            self.deselect = not elem[tuple(diff)[0].index].select
 
         context.window_manager.modal_handler_add(self)
         return {'RUNNING_MODAL'}
@@ -71,18 +98,21 @@ def register():
     wm = bpy.context.window_manager
     km = wm.keyconfigs.addon.keymaps.new(
         name='3D View', space_type='VIEW_3D')
+
     kmi = km.keymap_items.new(
         VIEW3D_OT_paint_select.bl_idname, 'LEFTMOUSE', 'PRESS')
     kmi.properties['extend'] = 0
     kmi.properties['toggle'] = 0
     kmi.properties['deselect'] = 0
     keymaps.append((km, kmi))
+
     kmi = km.keymap_items.new(
         VIEW3D_OT_paint_select.bl_idname, 'LEFTMOUSE', 'PRESS', shift=True)
     kmi.properties['extend'] = 1
     kmi.properties['toggle'] = 1
     kmi.properties['deselect'] = 0
     keymaps.append((km, kmi))
+
     kmi = km.keymap_items.new(
         VIEW3D_OT_paint_select.bl_idname, 'LEFTMOUSE', 'PRESS', ctrl=True,
         shift=True)
