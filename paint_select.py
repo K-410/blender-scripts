3ae3b195ccb9ae0ac2badd59b9eb1f0120c37a39
K-410 <iceythe@gmail.com>
2019-06-02 09:39:20 +0200

    Fix deselect when clicking empty space

--- paint_select.py
+++ paint_select.py
@@ -7,7 +7,7 @@ bl_info = {
     "name": "Paint Select",
     "description": (""),
     "author": "kaio",
-    "version": (1, 0, 0),
+    "version": (0, 0, 1),
     "blender": (2, 80, 0),
     "location": "3D View",
     "category": "3D View"
@@ -19,25 +19,24 @@ class VIEW3D_OT_paint_select(bpy.types.Operator):
     bl_label = "Paint Select"
     bl_options = {'REGISTER', 'UNDO'}
 
-    extend: BoolProperty(name="Extend", default=False)
-    deselect: BoolProperty(name="Deselect", default=False)
+    extend: BoolProperty(name="Add", default=False)
+    deselect: BoolProperty(name="Subtract", default=False)
     toggle: BoolProperty(name="Toggle", default=False)
+    deselect_all: BoolProperty(name="Deselect All", default=True)
 
     def modal(self, context, event):
 
         if event.type == 'MOUSEMOVE':
-            event_xy = event.mouse_region_x, event.mouse_region_y
+            mouse_co = event.mouse_region_x, event.mouse_region_y
             threshold = self.drag_threshold
-            init_xy = self.init_xy
-            z = zip(init_xy, event_xy)
+            init_mouse_co = self.init_mouse_co
+            z = zip(init_mouse_co, mouse_co)
 
             if not all(isclose(x, y, abs_tol=threshold) for x, y in z):
 
-                extend = True if not self.deselect else False
-
-                bpy.ops.view3d.select(
+                self.select(
                     'INVOKE_DEFAULT',
-                    extend=extend,
+                    extend=True if not self.deselect else False,
                     toggle=self.toggle,
                     deselect=self.deselect)
 
@@ -53,7 +52,8 @@ class VIEW3D_OT_paint_select(bpy.types.Operator):
         msm = ts.mesh_select_mode[:]
 
         self.drag_threshold = context.preferences.inputs.drag_threshold
-        self.init_xy = event.mouse_region_x, event.mouse_region_y
+        self.init_mouse_co = event.mouse_region_x, event.mouse_region_y
+        self.select = bpy.ops.view3d.select
 
         elem = None
 
@@ -67,22 +67,24 @@ class VIEW3D_OT_paint_select(bpy.types.Operator):
         if not elem:
             return {'CANCELLED'}
 
-        presel = set(el for el in elem if not el.select)
+        pre_sel = set(el for el in elem if not el.select)
 
-        bpy.ops.view3d.select(
+        self.select(
             'INVOKE_DEFAULT',
             extend=False,
             toggle=self.toggle,
-            deselect=self.deselect)
+            deselect=self.deselect,
+            deselect_all=self.deselect_all)
 
-        postsel = set(el for el in elem if not el.select)
+        post_sel = set(el for el in elem if not el.select)
 
-        diff = postsel - presel
+        diff = post_sel - pre_sel
         if not diff:
-            diff = presel - postsel
+            diff = pre_sel - post_sel
 
         elem.ensure_lookup_table()
         if self.toggle and diff:
+            # prevent initial elem from being toggled
             self.deselect = not elem[tuple(diff)[0].index].select
 
         context.window_manager.modal_handler_add(self)
