2dcaaceaf3933d24eb62006d66f7a7359e4e3100
kaio <iceythe@gmail.com>
2019-08-28 14:32:47 +0200

    minor updates
    use for instead of while in select
    fix tab skip

--- 2.8/text_toggle_comment.py
+++ 2.8/text_toggle_comment.py
@@ -21,10 +21,12 @@ class TEXT_OT_toggle_comment(bpy.types.Operator):
         return getattr(context.space_data, "text", False)
 
     def execute(self, context):
-        bpy_ops_text = bpy.ops.text
         txt = context.space_data.text
-        all_lines = txt.lines[:]
-        index = all_lines.index
+
+        def index(_line):
+            for i, line in enumerate(txt.lines):
+                if line == _line:
+                    return i
 
         # selection range as indices
         lin_a = txt.current_line_index
@@ -33,58 +35,40 @@ class TEXT_OT_toggle_comment(bpy.types.Operator):
         col_b = txt.select_end_character
 
         start, end = sorted((lin_a, lin_b))
-        sel = all_lines[start:end + 1]
+        sel = txt.lines[start:end + 1]
         self.txt = txt
 
         # select line if only one, otherwise commenting will fail
         if len(sel) == 1:
-            bpy_ops_text.select_line()
+            bpy.ops.text.select_line()
 
-        # favor commenting if mixed lines
         non_empty = [l for l in sel if l.body.strip()]
+        type = 'COMMENT'
+        comment = True
         if all(l.body.startswith("#") for l in non_empty):
-
             type = 'UNCOMMENT'
             comment = False
-        else:
-            type = 'COMMENT'
-            comment = True
 
-        bpy_ops_text.comment_toggle(type=type)
+        bpy.ops.text.comment_toggle(type=type)
         # nudge selection range due to comment
         col_a += 1 if col_a and comment else -1 if col_a else 0
         col_b += 1 if col_b and comment else -1 if col_b else 0
 
         # ensure caret doesn't jump, restore selection
-        prev = 'PREVIOUS_CHARACTER'
-        next = 'NEXT_CHARACTER'
-        up = 'PREVIOUS_LINE'
-        dn = 'NEXT_LINE'
-
-        while txt.current_line_index != lin_a:
-            cur = txt.current_line_index
-            bpy_ops_text.move(type=cur > lin_a and up or dn)
-
-        last = next_last = None
-        while txt.current_character != col_a:
-            cur = txt.current_character
-            # workaround for tab being treated as single character
-            if cur == next_last:
-                break
-            bpy_ops_text.move(type=cur > col_a and prev or next)
-            next_last, last = last, cur
-
-        while index(txt.select_end_line) != lin_b:
-            end = index(txt.select_end_line)
-            bpy_ops_text.move_select(type=end > lin_b and up or dn)
-
-        last = next_last = None
-        while txt.select_end_character != col_b:
-            end = txt.select_end_character
-            if end == next_last:
-                break
-            bpy_ops_text.move_select(type=end > col_b and prev or next)
-            next_last, last = last, end
+        move, select = bpy.ops.text.move, bpy.ops.text.move_select
+        left, right = 'PREVIOUS_CHARACTER', 'NEXT_CHARACTER'
+        up, down = 'PREVIOUS_LINE', 'NEXT_LINE'
+        indentation = txt.indentation
+        txt.indentation = 'TABS'
+        for _ in range(abs(txt.current_line_index - lin_a)):
+            move(type=up if txt.current_line_index > lin_a else down)
+        for _ in range(abs(txt.current_character - col_a)):
+            move(type=left if txt.current_character > col_a else right)
+        for _ in range(abs(index(txt.select_end_line) - lin_b)):
+            select(type=up if index(txt.select_end_line) > lin_b else down)
+        for _ in range(abs(txt.select_end_character - col_b)):
+            select(type=left if txt.select_end_character > col_b else right)
+        txt.indentation = indentation
 
         return {'FINISHED'}
 
