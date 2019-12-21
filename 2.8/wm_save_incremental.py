c5e5f529668ee9fca15386155c6c908f0a9c01a8
kaio <iceythe@gmail.com>
2019-12-21 10:10:43 +0100

    Update wm_save_incremental.py

--- 2.8/wm_save_incremental.py
+++ 2.8/wm_save_incremental.py
@@ -34,7 +34,7 @@ class WM_OT_save_incremental(bpy.types.Operator):
     props = bpy.ops.wm.save_as_mainfile.get_rna_type().properties
     ann = __annotations__ = {}
 
-    for p in "copy", "relative_remap", "compress":
+    for p in "copy", "relative_remap":
         kwargs = {"name": props[p].name,
                   "default": props[p].default,
                   "description": props[p].description}
@@ -71,14 +71,134 @@ class WM_OT_save_incremental(bpy.types.Operator):
             increment += 1
 
         fp += str(increment) + ext
+
+        prefs = context.preferences.addons[__name__].preferences
+        show_notification = prefs.show_notification
+
         bpy.ops.wm.save_as_mainfile('EXEC_DEFAULT', filepath=fp, **kwargs)
-        self.report({'INFO'}, f"Saved \"{_path.basename(fp)}\"")
+        
+        if show_notification:
+            self.report({'INFO'}, f"Saved \"{_path.basename(fp)}\"")
         return {'FINISHED'}
+
+
+def _update(self, context):
+    from bpy.types import TOPBAR_MT_file as cls
+    if self.show_in_file_menu:
+        elems = ["layout.operator(\"wm.save_incremental\")"]
+        find_string = "\"wm.save_as_mainfile\", text=\"Save Copy...\""
+        return inject(cls, find_string, elems)
+    _remove(cls)
+
+
+def _remove(cls):
+    draw = inject.namespace.get("draw")
+    if draw:
+        draw_funcs = cls.draw._draw_funcs
+        idx = draw_funcs.index(draw)
+        draw_funcs[idx] = cls._backup
+        del cls._backup
+
+
+class SaveIncrementalPreferences(bpy.types.AddonPreferences):
+    bl_idname = __name__
+
+    show_in_file_menu: bpy.props.BoolProperty(
+        name="Show in File Menu", default=True, update=_update)
+
+    show_notification: bpy.props.BoolProperty(
+        name="Show Notification", default=True)
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+        layout.scale_y = 1.25
+        split = layout.split(factor=0.6)
+        col = split.column()
+        # print()
+        col.prop(self, "show_notification")
+        col.prop(self, "show_in_file_menu")
+        col = split.column()
+
+
+def inject(cls, find_string, elems):
+    import inspect
+
+    src = inject.src = getattr(inject, "src", None)
+    # print(repr(src))
+    if src is None:
+        try:
+            _src = inspect.getsource(cls.draw)
+            src = inject.src = _src.splitlines()
+        except (TypeError, OSError):
+            print("%s: Invalid draw function in %s" % (__name__, cls))
+            return
+
+        def indent_get(body):
+            for idx, char in enumerate(body):
+                if char != " ":
+                    return idx
+            return 0
+
+        def find(contents, string):
+            for idx, line in enumerate(contents):
+                if string in line:
+                    return idx
+            raise ValueError
+
+        for idx, line in enumerate(src):
+            if line.find("def draw(self, context):") != -1:
+                start = next(idx for (idx, c) in enumerate(line) if c != " ")
+            if line.find(find_string) != -1:
+                indent2 = " " * indent_get(line)
+                for elem in reversed(elems):
+                    src.insert(idx + 1, indent2 + elem)
+                break
+        else:
+            print("%s: Couldn't inject, string not found" % __name__)
+            return
+
+        inject.namespace = {}
+        inject.start = start
+
+    start = inject.start
+    namespace = inject.namespace
+    namespace.update(vars(inspect.getmodule(cls)))
+    new_src = "\n".join(line[start:] for line in src)
+    exec(new_src, namespace)
+
+    draw_funcs = getattr(cls.draw, "_draw_funcs", None)
+    if draw_funcs is None:
+        def _draw(self, context):
+            pass
+        cls.append(_draw)
+        cls.draw._draw_funcs.remove(_draw)
+        draw_funcs = cls.draw._draw_funcs
+
+    for idx, func in enumerate(draw_funcs):
+
+        if func.__module__ == cls.__module__ and \
+           func not in func.__globals__.values():
+
+            cls._backup = func
+            draw_funcs[idx] = namespace['draw']
+            break
+    else:
+        raise
 
 
 def register():
     bpy.utils.register_class(WM_OT_save_incremental)
+    bpy.utils.register_class(SaveIncrementalPreferences)
+
+    context = bpy.context
+    prefs = context.preferences.addons[__name__].preferences
+
+    _update(prefs, context)
 
 
 def unregister():
+    cls = bpy.types.TOPBAR_MT_file
+    _remove(cls)
+    bpy.utils.unregister_class(SaveIncrementalPreferences)
     bpy.utils.unregister_class(WM_OT_save_incremental)
